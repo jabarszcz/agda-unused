bcfaf4b6ac9eb429aad2eb13ddc34a067d88b72e
Jean-Alexandre Barszcz <jean-alexandre.barszcz@umontreal.ca>
2026-05-19 15:58:39 -0400

    [WIP] Test with other GHCs

--- flake.nix
+++ flake.nix
@@ -6,19 +6,43 @@
   let
 
     packages = nixpkgs.legacyPackages.x86_64-linux;
-    derivation = packages.haskellPackages.callCabal2nix "agda-unused" self {};
+
+    # GHC versions to test against.
+    # Adjust when nixpkgs adds or drops GHC versions.
+    ghcVersions = {
+      ghc94  = packages.haskell.packages.ghc94;
+      ghc96  = packages.haskell.packages.ghc96;
+      ghc98  = packages.haskell.packages.ghc98;
+      ghc910 = packages.haskellPackages;
+    };
+
+    mkDrv = hpkgs: hpkgs.callCabal2nix "agda-unused" self {};
+
+    mkShell = hpkgs: (mkDrv hpkgs).env.overrideAttrs (oldAttrs: {
+      buildInputs = oldAttrs.buildInputs ++ [
+        packages.cabal-install
+        hpkgs.Agda
+      ];
+    });
+
+    derivation = mkDrv packages.haskellPackages;
 
   in
 
   {
     defaultPackage.x86_64-linux = derivation;
 
-    devShell.x86_64-linux = derivation.env.overrideAttrs (oldAttrs: {
-      buildInputs = oldAttrs.buildInputs ++ [
-        packages.cabal-install
-        packages.haskellPackages.Agda
-      ];
-    });
+    devShells.x86_64-linux =
+      { default = mkShell packages.haskellPackages; }
+      // builtins.mapAttrs (_: mkShell) ghcVersions;
+
+    # nix flake check builds only the default GHC.
+    checks.x86_64-linux.default = derivation;
+
+    # Full matrix: nix build .#matrix.x86_64-linux.ghc96
+    matrix.x86_64-linux = builtins.mapAttrs (_: mkDrv) ghcVersions;
+
+    # GHC+deps bundles for consumption by other flakes.
+    ghcWithDeps.x86_64-linux = builtins.mapAttrs (_: mkShell) ghcVersions;
   };
 }
-
